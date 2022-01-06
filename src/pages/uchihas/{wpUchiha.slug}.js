3593926da9c2c840bd427598e25424cb05591ffa
Baysal Naim (s109462) <naim.baysal@student.ap.be>
2022-01-06 19:35:47 +0100

    Artist template page: opmaak en styling

--- src/pages/uchihas/{wpUchiha.slug}.js
+++ src/pages/uchihas/{wpUchiha.slug}.js
@@ -2,24 +2,63 @@ import * as React from 'react'
 import { graphql } from 'gatsby'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image' 
 import Layout from '../../components/layout'
+import {
+  header,
+  headerInfo,
+  uchihaName,
+  headerPicture,
+  fullName,
+  uchihaEyes,
+  uchihaDescription,
+  uchihaInfo,
+  uchihaPicture,
+  uchihaPictures,
+} from "../../page.module.css"
+
+
 const UchihaPage = ({
   data: {
-    wpUchiha: { uchihaFields: uchiha },
+    wpUchiha: {
+      uchihaFields: uchiha,
+      eyes: {nodes: eyes},
+    },
   },
 }) => {
   const image = getImage(uchiha.picture.localFile)
+  const picture1 = getImage(uchiha.pictures.picture1.localFile)
+  const picture2 = getImage(uchiha.pictures.picture2.localFile)
+  const picture3 = getImage(uchiha.pictures.picture3.localFile)
   return (
     <Layout pageTitle="Uchiha Template">
-    <div>
-      <GatsbyImage image={image} alt={uchiha.picture.altText} />
-      <h1>{uchiha.firstName} {uchiha.lastName}</h1>
-      <div dangerouslySetInnerHTML={{__html: uchiha.description}} />
-      <p>Age: {uchiha.age}</p>
-      <p>Weight: {uchiha.weight}</p>
-      <p>Height: {uchiha.height}</p>
-      <p>Jutsu Speciality: {uchiha.jutsuSpeciality}</p>
-      <p>Status: {uchiha.status}</p>
+    <div className={header}>
+      <div className={headerInfo}>
+      {uchiha.firstName && <h3 className={uchihaName}>{uchiha.lastName}</h3>}
+          <div className={uchihaEyes}>
+            {eyes.map((eye, i) => (
+              <span key={i}>
+                {eye.name} {i + 1 < eyes.length && "- "}
+              </span>
+            ))}
+          </div>
+      <h1 className={fullName}>{uchiha.firstName} {uchiha.lastName}</h1>
+      <div
+        className={uchihaDescription} 
+        dangerouslySetInnerHTML={{__html: uchiha.description}} />
+      <p><span className={uchihaInfo}>Age:</span> {uchiha.age}</p>
+      <p><span className={uchihaInfo}>Weight:</span> {uchiha.weight}</p>
+      <p><span className={uchihaInfo}>Height:</span> {uchiha.height}</p>
+      <p><span className={uchihaInfo}>Jutsu Speciality:</span> {uchiha.jutsuSpeciality}</p>
+      <p><span className={uchihaInfo}>Status:</span> {uchiha.status}</p>
+      </div>
+      <GatsbyImage 
+        className={headerPicture}
+        image={image} alt={uchiha.picture.altText} />
     </div>
+    <div className={uchihaPictures}>
+        <GatsbyImage className={uchihaPicture} image={picture1} alt={uchiha.pictures.picture1.altText} />
+        <GatsbyImage className={uchihaPicture} image={picture2} alt={uchiha.pictures.picture2.altText} />
+        <GatsbyImage className={uchihaPicture} image={picture3} alt={uchiha.pictures.picture3.altText} />
+      </div>
     </Layout>
   )
 }
@@ -34,8 +73,9 @@ query ($id: String) {
       description
       height
       weight
-      jutsuSpeciality
       status
+      jutsuSpeciality
+      fieldGroupName
       picture {
         localFile {
           childImageSharp {
@@ -44,9 +84,38 @@ query ($id: String) {
         }
         altText
       }
+      pictures {
+        picture1 {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED)
+            }
+          }
+        }
+        picture2 {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED)
+            }
+          }
+        }
+        picture3 {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED)
+            }
+          }
+        }
+      }
+    }
+    eyes {
+      nodes {
+        name
+      }
     }
   }
 }
+
 
 `
 
